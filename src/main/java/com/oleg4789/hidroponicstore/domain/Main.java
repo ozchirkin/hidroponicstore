57c7077f95137a4595c0d6adc00e7af1b9e60c92
ozchirkin <oleg.chirkin47@gmail.com>
2020-04-05 23:50:51 +0600

    Изменен .gitignore;

--- src/main/java/com/oleg4789/hidroponicstore/domain/Main.java
+++ src/main/java/com/oleg4789/hidroponicstore/domain/Main.java
@@ -1,13 +1,5 @@
 package com.oleg4789.hidroponicstore.domain;
 
-import com.oleg4789.hidroponicstore.test.Car;
-
-import java.lang.reflect.Array;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         User myPerson = new User.Builder()
